171e57f3f42e2fbc8b22f541424f6c0b88167620
Susmitha Mudigonda <51853078+Sushmitha2708@users.noreply.github.com>
2020-08-17 13:00:14 +0530

    Delete sample.py

--- Python/sample.py
+++
@@ -1,6 +0,0 @@
-from pyspark import SparkContext
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.appName("simpleapp").getOrCreate() # Spark session is created
-print("Hey!")
-spark.stop()
