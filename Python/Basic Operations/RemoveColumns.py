f7447cc19090291d390dd2d9a87129089bf9497f
Susmitha Mudigonda <51853078+Sushmitha2708@users.noreply.github.com>
2020-08-29 19:44:46 +0530

    Update RemoveColumns.py

--- Python/Basic Operations/RemoveColumns.py
+++ Python/Basic Operations/RemoveColumns.py
@@ -1,9 +1,9 @@
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName("Python Spark SQL basic example").getOrCreate()
+spark = SparkSession.builder.appName("Pyspark example").getOrCreate()
 
 
-df = spark.read.format("json").load("F:/Spark/Python/Basic Operations/data/sample_data1.json")
+df = spark.read.format("json").load("/data/sample_data1.json")
 df.createOrReplaceTempView("dftable")
 
 df.drop("ORIGIN_COUNTRY_NAME").columns
